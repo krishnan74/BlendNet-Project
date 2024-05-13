61b0a9eca45a238ca601f147df7af0a82fc2e968
krishnan74 <divyakrishnan.r74@gmail.com>
2024-05-13 17:52:19 +0530

    chore: Remove unused Django settings and views

--- urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('getAllStocks/', views.getAllStocks, name='stocks'),
-    path('getStockDetails/', views.getStockDetails, name='stock_details'),
-]
